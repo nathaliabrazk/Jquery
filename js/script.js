30c25c263571f4030a31e4546711a8369458c641
nathaliabrazk <111463766+nathaliabrazk@users.noreply.github.com>
2023-12-08 14:53:43 -0300

    Finalização

--- js/script.js
+++ js/script.js
@@ -13,4 +13,18 @@ $(document).ready(function(){
     $("#toggleBtn").click(function(){
         $("#content").fadeToggle();
     });
+
+    // Efeito de rotação
+    $("#rotateBtn").on('click', function(){
+        // Verifica se a imagem já foi rotacionada
+        if ($("#rotatingImage").data('rotated')) {
+            // Se sim, reverte a rotação
+            $("#rotatingImage").animate({rotate: '0deg'}, 1000);
+            $("#rotatingImage").data('rotated', false);
+        } else {
+            // Se não, aplica a rotação
+            $("#rotatingImage").animate({rotate: '+=360deg'}, 1000);
+            $("#rotatingImage").data('rotated', true);
+        }
+    });
 });
